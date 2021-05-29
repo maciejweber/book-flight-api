0c754f9c711131c1c182b0abe15031e293b34918
Maciej Weber <maciejwebber@gmail.com>
2021-05-29 13:40:51 +0200

    filters in flight, display name in cities and countries

--- countries/models.py
+++ countries/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 class Country(models.Model):
     name = models.CharField("Country name", max_length=30)
+    display_name = models.CharField(
+        "Display name", max_length=30, help_text="Display name should stary with a capital letter")
 
     def __str__(self) -> str:
-        return self.name
+        return self.display_name
 
     class Meta:
         verbose_name = "Country"
